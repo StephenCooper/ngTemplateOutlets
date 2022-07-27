0a850ba86aecd8d6f00673bf7464f504126ad313
Stephen Cooper <scooperdev@gmail.com>
2022-07-27 16:51:14 +0100

    v11 to v12

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -49,7 +49,7 @@
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import "zone.js/dist/zone"; // Included with Angular CLI.
+import "zone.js"; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
