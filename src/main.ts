d76dcb2b00c3595871b175e3dcd363bcc9d70fc8
Stephen Cooper <scooperdev@gmail.com>
2022-07-27 17:01:33 +0100

    Fix compilation errors

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,6 @@ import { AppModule } from "./app/app.module";
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then(ref => {
-    // Ensure Angular destroys itself on hot reloads.
-    if (window["ngRef"]) {
-      window["ngRef"].destroy();
-    }
-    window["ngRef"] = ref;
 
     // Otherwise, log the boot error
   })
